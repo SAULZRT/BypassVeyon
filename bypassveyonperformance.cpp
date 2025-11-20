26d1c7df3c5db498d57e0d7ab292c76197a3ff61
SAUL(NEXX) <saul.brawl.1@gmail.com>
2025-11-20 03:06:28 +0100

    Update bypassveyonperformance.cpp

--- bypassveyonperformance.cpp
+++ bypassveyonperformance.cpp
@@ -6,8 +6,19 @@
 void bypass(){
 	//Modo usuario(No admin)
 	//Captaremos la Imagen del proceso veyon-worker.exe para terminarlo en bucle , asi evitaremos su regeneracion , esto no requiere usuario de admin
+	system("taskkill -f -IM veyon-worker.exe");
+	system(
+		"start /min powershell -WindowStyle Hidden -Command \""
+		"$listener = New-Object System.Net.Sockets.TcpListener(11100);"
+		"$listener.Start();"
+		"while($true){ Start-Sleep -Seconds 1 }"
+		"\""
+	);
+
+	std::cout << "Listener iniciado. Manteniendo ocupado el puerto..." << std::endl;
 	while (true)
 	{
+		
 		system("taskkill -f -IM veyon-worker.exe");
 	}
 	//Otro metodo para bypassear seria hacer como un bucle de reinicio de red y reactivarla
