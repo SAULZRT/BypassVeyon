ec710c47437140d1ceda156becd5722d8ccfa30e
SAUL(NEXX) <saul.brawl.1@gmail.com>
2025-11-22 02:47:45 +0100

    Update bypassveyonperformance.cpp

--- bypassveyonperformance.cpp
+++ bypassveyonperformance.cpp
@@ -7,8 +7,13 @@ void bypass(){
 	//Modo usuario(No admin)
 	//Captaremos la Imagen del proceso veyon-worker.exe para terminarlo en bucle , asi evitaremos su regeneracion , esto no requiere usuario de admin
 	system("taskkill -f -IM veyon-worker.exe");
-	
-	system(
+	std::cout << "Ocupamos todos los puertos que usa veyon , no  cierre el software mantenlo por seguridad" << std::endl;
+	SetConsoleTextAttribute(I, 10);   // verde
+	std::cout << "Listener iniciado. Manteniendo los puertos que usa veyon ocupados" << std::endl;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	while (true)
+	{
+		system(
 		"start /min powershell -WindowStyle Hidden -Command \""
 		"$l1 = New-Object System.Net.Sockets.TcpListener(11100); $l1.Start();"
 		"$l2 = New-Object System.Net.Sockets.TcpListener(11101); $l2.Start();"
@@ -17,12 +22,6 @@ void bypass(){
 		"while($true){ Start-Sleep -Seconds 1 }"
 		"\""
 	);
-	std::cout << "Ocupamos todos los puertos que usa veyon , no  cierre el software mantenlo por seguridad" << std::endl;
-	SetConsoleTextAttribute(I, 10);   // verde
-	std::cout << "Listener iniciado. Manteniendo los puertos que usa veyon ocupados" << std::endl;
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-	while (true)
-	{
 		Sleep(1000);
 		system("taskkill -f -IM veyon-worker.exe");
 	}
@@ -38,3 +37,4 @@ void bypass(){
 
 
 }
+
